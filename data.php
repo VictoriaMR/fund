52adc09865a472f3dfec2c522c3a195fbd03d641
LiaoMingRong <849376723@qq.com>
2020-12-01 16:27:44 +0800

    debug

--- data.php
+++ data.php
@@ -7,10 +7,10 @@ $codeArr = json_decode($param, true);
 if (empty($codeArr)) {
 	exit('12312');
 }
-$he = array_sum(array_keys($codeArr));
 $result = ['list'=>[], 'total' => 0];
 foreach ($codeArr as $key => $code) {
-	$data = file_get_contents('http://fundgz.1234567.com.cn/js/'.$key.'.js?rt='.time());
+	$data = @file_get_contents('http://fundgz.1234567.com.cn/js/'.$key.'.js?rt='.time());
+	if (empty($data)) continue;
 	$data = json_decode(rtrim(ltrim($data, 'jsonpgz('), ');'), true);
 	$result['list'][] = [
 		'name' => $data['name'],
@@ -20,6 +20,14 @@ foreach ($codeArr as $key => $code) {
 		'win' => sprintf('%.2f', $code['free'] * $data['gszzl'] / 100),
 	];
 }
-$result['total'] = sprintf('%.2f', array_sum(array_column($result['list'], 'win')));
+if (!empty($result['list'])) {
+	$he = array_sum(array_column($result['list'], 'free'));
+	$result['total'] = sprintf('%.2f', array_sum(array_column($result['list'], 'win')));
+	$result['total_percent'] = sprintf('%.2f', $result['total'] / $he * 100);
+}
+$result['can_update'] = false;
+if (date('H') >= 15) {
+	$result['can_update'] = true;
+}
 echo json_encode($result);
 exit();
